48e9a67964207293dc158d2f38f0cf26c757b0ae
Hoa Pham <rzz934@my.utsa.edu>
2017-10-27 00:40:51 -0500

    Separate RecipeKeeper.java into driver package

--- RK/src/driver/RecipeKeeper.java
+++ RK/src/driver/RecipeKeeper.java
@@ -1,4 +1,4 @@
-package view;
+package driver;
 	
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -13,9 +13,9 @@ public class RecipeKeeper extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Parent root = FXMLLoader.load(getClass().getResource("NewInterface.fxml"));
+			Parent root = FXMLLoader.load(getClass().getResource("/view/NewInterface.fxml"));
 			Scene scene = new Scene(root, 529, 737);
-			scene.getStylesheets().add(getClass().getResource("RecipeKeeper.css").toExternalForm());
+			scene.getStylesheets().add(getClass().getResource("/view/RecipeKeeper.css").toExternalForm());
 			primaryStage.setTitle("Interface Demo");
 			primaryStage.setScene(scene);
 			primaryStage.show();
