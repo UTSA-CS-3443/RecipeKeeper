86ae135b9cdbb865e6c92e1bfd5d52aa4f3fad80
Loc Nguyen <beastboy241@gmail.com>
2017-10-31 13:08:46 -0500

    Write Data class

---
+++ RK/src/model/WriteData.java
@@ -0,0 +1,51 @@
+package model;
+
+import java.io.FileWriter;
+
+public class WriteData {
+
+	private static final String COMMA_DELIMITER = ",";
+	private static final String NEW_LINE_SEPARATOR = "\n";
+	private static final String QUOTE_SEPARATOR = "\"";
+	
+	/**
+	 * default constructor
+	 */
+	public WriteData() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * Constructor 
+	 * @param recipeName.csv
+	 * @param Recipe
+	 */
+	public WriteData(String fileName, Recipe recipe) {
+		FileWriter fileWriter = null;
+		try {
+			fileWriter = new FileWriter(fileName);
+			for (Ingredient i : recipe.getIngredients()) {
+				fileWriter.append(i.getName());
+				fileWriter.append(COMMA_DELIMITER);
+				fileWriter.append(String.valueOf(i.getQuantity()));
+				fileWriter.append(COMMA_DELIMITER);
+				fileWriter.append(i.getUnit());
+				fileWriter.append(NEW_LINE_SEPARATOR);
+			}
+			fileWriter.append(COMMA_DELIMITER + NEW_LINE_SEPARATOR);
+			for (String c : recipe.getCategories()) {
+				fileWriter.append(c + " ");
+			}
+			fileWriter.append(NEW_LINE_SEPARATOR + COMMA_DELIMITER);
+			fileWriter.append(QUOTE_SEPARATOR);
+			fileWriter.append(recipe.getInstructions());
+			fileWriter.append(QUOTE_SEPARATOR);
+			AlertBox.display("Recipe Keeper", "Your recipe is saved.");
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+}
