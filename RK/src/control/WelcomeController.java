7273782704a03fae253498f62e9aa8b954df5039
Hoa Pham <rzz934@my.utsa.edu>
2017-11-02 01:41:37 -0500

    Add implementation to WelcomeController and its fxml

--- RK/src/control/WelcomeController.java
+++ RK/src/control/WelcomeController.java
@@ -1,11 +1,17 @@
 package control;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
 /**
  * Welcome Controller
@@ -13,6 +19,9 @@ import javafx.scene.control.Button;
  *
  */
 public class WelcomeController implements Initializable {
+	
+	@FXML // fx:id="motherPane"
+	BorderPane motherPane = new BorderPane();
 	
 	@FXML // fx:id="startRep"
 	Button startRep = new Button();
@@ -23,12 +32,45 @@ public class WelcomeController implements Initializable {
 	@FXML // fx:id="editRep"
 	Button editRep = new Button();
 	
+	// constant values
+	static Constants constants = new Constants();
 	
+	// minimum size of the window
+	private static final int[] MIN_SIZES = constants.getMinSizes();
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		// TODO Auto-generated method stub
 		
+		/**
+		 * Start a new recipe
+		 */
+		startRep.setOnAction( action -> {
+			try {
+				String fxmlFileDir = "/view/EditInterface.fxml";
+				String cssFileDir = "/view/RecipeKeeper.css";
+				Parent root = FXMLLoader.load(getClass().getResource(fxmlFileDir));
+				Scene editWindow = new Scene(root, MIN_SIZES[0], MIN_SIZES[1]);
+				editWindow.getStylesheets().add(getClass().getResource(cssFileDir).toExternalForm());
+				Stage originalStage = (Stage) motherPane.getScene().getWindow();
+				originalStage.setScene(editWindow);
+			} catch (IOException ioe) {
+				
+			}
+		});
+		
+		/**
+		 * find a recipe and enter read mode
+		 */
+		findRep.setOnAction(action -> {
+			
+		});
+		
+		/**
+		 * find a recipe and enter edit mode
+		 */
+		editRep.setOnAction(action -> {
+			
+		});
 	}
 
 }
