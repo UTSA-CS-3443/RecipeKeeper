c7e7bf808d621b58a7a95573c0ab41768c2be393
diitor <urbaneshouter@gmail.com>
2017-11-01 20:11:38 -0500

    Added a readRecipe method to read recipe files from a "Recipe" directory

--- RK/src/control/ReadController.java
+++ RK/src/control/ReadController.java
@@ -1,8 +1,13 @@
 package control;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.ResourceBundle;
 
 import javafx.stage.Stage;
@@ -26,6 +31,7 @@ import javafx.scene.layout.BorderPane;
 import model.AlertBox;
 import model.Ingredient;
 import model.Recipe;
+import model.RecipeList;
 
 /**
  * Controller for Reading interface
@@ -116,6 +122,47 @@ public class ReadController implements Initializable {
 				// do some more stuffs with ingredients
 			}
 		});
+	}
+	
+	public RecipeList readRecipes() {
+		File dir = new File("./Recipes");
+		File[] recipeFiles = dir.listFiles();
+		BufferedReader br = null;
+		String line = "";
+		String newInstructions = "";
+		Recipe newRecipe = new Recipe();
+		ArrayList<Recipe> newRecipeArray = new ArrayList<Recipe>();
+		ArrayList<Ingredient> newIngredientArray = new ArrayList<Ingredient>();
+		ArrayList<String> newCategories = new ArrayList<String>();
+		Ingredient newIngredient;
+		if (recipeFiles != null){
+			for ( File child : recipeFiles){
+				try {
+					br = new BufferedReader(new FileReader(child));
+					while ((line = br.readLine()) != null && !line.equals(",")){
+						String[] lines = line.split(",");
+						newIngredient = new Ingredient(lines[0], Double.parseDouble(lines[1]), lines[2]);
+						newIngredientArray.add(newIngredient);
+					}
+					while ((line = br.readLine()) != null && !line.equals(",")){
+						String[] lines = line.split(",");
+						newCategories.addAll(Arrays.asList(lines));
+					}
+					while ((line = br.readLine()) != null){
+						newInstructions += line;
+					}
+					newRecipe.setName(child.getName());
+					newRecipe.setIngredients(newIngredientArray);
+					newRecipe.setCategories(newCategories);
+					newRecipe.setInstructions(newInstructions);
+					newRecipeArray.add(newRecipe);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+				}
+			}
+		}
+		RecipeList recipeList = new RecipeList(newRecipeArray);
+		return recipeList;
 	}
 
 }
