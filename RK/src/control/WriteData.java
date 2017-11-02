6eeea5e1fb75c673aa86f981651ca914fdc0aa64
Hoa Pham <rzz934@my.utsa.edu>
2017-11-02 10:16:47 -0500

    Simplify WriteData.java

--- RK/src/control/WriteData.java
+++ RK/src/control/WriteData.java
@@ -7,10 +7,6 @@ import model.Ingredient;
 import model.Recipe;
 
 public class WriteData {
-
-	private static final String COMMA_DELIMITER = ",";
-	private static final String NEW_LINE_SEPARATOR = "\n";
-	private static final String QUOTE_SEPARATOR = "\"";
 	
 	/**
 	 * default constructor
@@ -29,23 +25,7 @@ public class WriteData {
 		FileWriter fileWriter = null;
 		try {
 			fileWriter = new FileWriter(fileName);
-			for (Ingredient i : recipe.getIngredients()) {
-				fileWriter.append(i.getName());
-				fileWriter.append(COMMA_DELIMITER);
-				fileWriter.append(String.valueOf(i.getQuantity()));
-				fileWriter.append(COMMA_DELIMITER);
-				fileWriter.append(i.getUnit());
-				fileWriter.append(NEW_LINE_SEPARATOR);
-			}
-			fileWriter.append(COMMA_DELIMITER + NEW_LINE_SEPARATOR);
-			for (String c : recipe.getCategories()) {
-				fileWriter.append(c + " ");
-			}
-			fileWriter.append(NEW_LINE_SEPARATOR + COMMA_DELIMITER);
-			fileWriter.append(QUOTE_SEPARATOR);
-			fileWriter.append(recipe.getInstructions());
-			fileWriter.append(QUOTE_SEPARATOR);
-			AlertBox.display("Recipe Keeper", "Your recipe is saved.");
+			fileWriter.append(recipe.toString());
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
